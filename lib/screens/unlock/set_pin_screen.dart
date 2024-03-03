7f44e9e6f6e4d3bd715dcc676ccae4d4c8c14137
Rana Talha <ranatalha0097@gmail.com>
2024-03-03 16:31:46 +0000

    3 march 2024

--- lib/screens/unlock/set_pin_screen.dart
+++ lib/screens/unlock/set_pin_screen.dart
@@ -4,7 +4,7 @@ import 'package:sizer/sizer.dart';
 import '../../constants/colors.dart';
 
 class PinScreen extends StatefulWidget {
-  // const PinScreen({super.key});
+  const PinScreen({Key? key}) : super(key: key);
 
   @override
   State<PinScreen> createState() => _PinScreenState();
@@ -13,6 +13,7 @@ class PinScreen extends StatefulWidget {
 class _PinScreenState extends State<PinScreen> {
   String selectedNumber = '';
   List<String> numbers = List.generate(6, (index) => '');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,8 +40,7 @@ class _PinScreenState extends State<PinScreen> {
             // color: Colors.blue,
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.sp),
-              child:
-              Row(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
@@ -97,7 +97,10 @@ class _PinScreenState extends State<PinScreen> {
                     children: [
                       digitBox(''),
                       digitBox('0'),
-                      digitBox(''),
+                      digitBox(
+                        '',
+                        imagePath: 'assets/images/remove_button.png',
+                      ),
                     ],
                   ),
                   // SizedBox(height: 2.h,)
@@ -123,22 +126,21 @@ class _PinScreenState extends State<PinScreen> {
 
   Widget circularTextField(int index) {
     return GestureDetector(
-      onTap: () {
-        _showKeyboard(context, index);
-      },
       child: Container(
         height: 5.h,
         width: 5.h,
         decoration: BoxDecoration(
+          border: Border.all( color: numbers[index] != "" ? AppColors.activeButtonColor:Colors.transparent,
+          ),
           borderRadius: BorderRadius.circular(40),
-          color: Colors.grey.withOpacity(0.2),
+          color: numbers[index] != "" ? Colors.transparent:Colors.grey.withOpacity(0.2),
         ),
         child: Center(
           child: Text(
             numbers[index],
             style: TextStyle(
               fontSize: 24.sp,
-              color: Colors.black,
+              color: AppColors.activeButtonColor,
               fontWeight: FontWeight.w400,
               fontFamily: 'ArialASDCF',
             ),
@@ -148,79 +150,50 @@ class _PinScreenState extends State<PinScreen> {
     );
   }
 
-  void _showKeyboard(BuildContext context, int index) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Container(
-          height: 300.h,
-          child: Column(
-            children: [
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 3,
-                  children: List.generate(
-                    9,
-                        (i) {
-                      final number = (i + 1) % 10; // Display 1-9 and 0
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            numbers[index] = number.toString();
-                          });
-                          Navigator.pop(context); // Close the keyboard
-                        },
-                        child: Container(
-                          alignment: Alignment.center,
-                          margin: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.blue,
-                          ),
-                          child: Text(
-                            number.toString(),
-                            style: TextStyle(fontSize: 24.sp, color: Colors.white),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
   Widget pinBox(String number) {
     return Container(
-      height: 5.h,
-      width: 5.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(40),
-        color: AppColors.textColorGreyShade2.withOpacity(0.05),
-      ),
-   child: Center(
-    child: Text(
-    number,
-    style: TextStyle(
-    fontSize: 24.sp,
-    color: number.isNotEmpty ? AppColors.textColorWhite : Colors.black, // Adjust color based on selection
-    fontWeight: FontWeight.w400,
-    fontFamily: 'ArialASDCF',
-    ),
-    ))
-    );
+        height: 5.h,
+        width: 5.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: AppColors.textColorGreyShade2.withOpacity(0.05),
+        ),
+        child: Center(
+            child: Text(
+          number,
+          style: TextStyle(
+            fontSize: 24.sp,
+            color: number.isNotEmpty
+                ? AppColors.textColorWhite
+                : Colors.black, // Adjust color based on selection
+            fontWeight: FontWeight.w400,
+            fontFamily: 'ArialASDCF',
+          ),
+        )));
   }
 
-  Widget digitBox(String number) {
+  Widget digitBox(String number, {String? imagePath}) {
     return GestureDetector(
       onTap: () {
-        setState(() {
-          selectedNumber = number;
-        });
+        imagePath == null
+            ? setState(() {
+                // Append the pressed digit to the pin boxes
+                for (int i = 0; i < numbers.length; i++) {
+                  if (numbers[i].isEmpty) {
+                    numbers[i] = number;
+                    break; // Stop after adding the digit to the first empty box
+                  }
+                }
+              })
+            : setState(() {
+                // Remove the last entered digit
+                for (int i = numbers.length - 1; i >= 0; i--) {
+                  if (numbers[i].isNotEmpty) {
+                    numbers[i] = '';
+                    break;
+                  }
+                }
+              });
       },
       child: Container(
         height: 5.h,
@@ -230,15 +203,21 @@ class _PinScreenState extends State<PinScreen> {
             // color:Colors.white.withOpacity(0.5),
             ),
         child: Center(
-          child: Text(
-            number,
-            style: TextStyle(
-              fontSize: 24.sp,
-              color: AppColors.textColorWhite,
-              fontWeight: FontWeight.w400,
-              fontFamily: 'ArialASDCF',
-            ),
-          ),
+          child: imagePath == null
+              ? Text(
+                  number,
+                  style: TextStyle(
+                    fontSize: 24.sp,
+                    color: AppColors.textColorWhite,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'ArialASDCF',
+                  ),
+                )
+              : Image.asset(
+                  imagePath,
+                  height: 13.sp,
+                  width: 13.sp,
+                ),
         ),
       ),
     );
