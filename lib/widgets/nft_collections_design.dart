27bca1970b3aa9c91fe5808e5e62384ec3148694
Rana Talha <ranatalha0097@gmail.com>
2024-06-03 22:27:16 +0500

    3 june 2024

--- lib/widgets/nft_collections_design.dart
+++ lib/widgets/nft_collections_design.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
@@ -24,25 +26,23 @@ class _NftsCollectionDesignState extends State<NftsCollectionDesign> {
   Widget build(BuildContext context) {
     print('collectionId' + widget.nftsCollection.collectionId);
     return GestureDetector(
-      onTap: () =>
-          Navigator.of(context).pushNamed(NftsCollectionDetails.routeName,
-              arguments: {
-                'collectionName': widget.nftsCollection.collectionName,
-                'collectionId': widget.nftsCollection.collectionId,
-                'creatorId': widget.nftsCollection.creatorId,
-                'creatorRoyalty': widget.nftsCollection.creatorRoyalty,
-                'ownerId': widget.nftsCollection.ownerId,
-                'nftIds': widget.nftsCollection.nftIds.length.toString(),
-                'standard': widget.nftsCollection.collectionStandard.toString(),
-                'chain': widget.nftsCollection.chain.toString(),
-                'createdAt': widget.nftsCollection.createdAt.toString(),
-                'collectionStatus': widget.nftsCollection.status.toString(),
-                'listingType': widget.nftsCollection.listingType.toString(),
-                'image': widget.nftsCollection.image.toString(),
-                'logoLink': widget.nftsCollection.logo.toString(),
-                'bannerLink': widget.nftsCollection.banner.toString(),
-              }
-          ),
+      onTap: () => Navigator.of(context)
+          .pushNamed(NftsCollectionDetails.routeName, arguments: {
+        'collectionName': widget.nftsCollection.collectionName,
+        'collectionId': widget.nftsCollection.collectionId,
+        'creatorId': widget.nftsCollection.creatorId,
+        'creatorRoyalty': widget.nftsCollection.creatorRoyalty,
+        'ownerId': widget.nftsCollection.ownerId,
+        'nftIds': widget.nftsCollection.nftIds.length.toString(),
+        'standard': widget.nftsCollection.collectionStandard.toString(),
+        'chain': widget.nftsCollection.chain.toString(),
+        'createdAt': widget.nftsCollection.createdAt.toString(),
+        'collectionStatus': widget.nftsCollection.status.toString(),
+        'listingType': widget.nftsCollection.listingType.toString(),
+        'image': widget.nftsCollection.image.toString(),
+        'logoLink': widget.nftsCollection.logo.toString(),
+        'bannerLink': widget.nftsCollection.banner.toString(),
+      }),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: Stack(
@@ -50,9 +50,13 @@ class _NftsCollectionDesignState extends State<NftsCollectionDesign> {
             Container(
               // color: Colors.red,
               // decoration: BoxDecoration(color: AppColors.textColorGreyShade2.withOpacity(0.25)),
-              height: 20.h,
-              child: Image.network(widget.nftsCollection.image!, fit: BoxFit.cover,
-                errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+              height: 27.2.h,
+              child: Image.network(
+                widget.nftsCollection.image!,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                errorBuilder: (BuildContext context, Object exception,
+                    StackTrace? stackTrace) {
                   return Image.asset(
                     'assets/images/nft.png', // Path to your placeholder image
                     fit: BoxFit.cover,
@@ -65,100 +69,134 @@ class _NftsCollectionDesignState extends State<NftsCollectionDesign> {
                 bottom: 0,
                 left: 0,
                 right: 0,
-                child: Container(
-                  height: 7.2.h,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
-                    color: AppColors.textColorBlack.withOpacity(0.3),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
                   ),
-                  child:
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.sp, right: 10.sp),
-                    child: Row(
-                      // mainAxisAlignment: MainAxisAlignment.start,
-                      // crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Container(
-                        //   height: 29.sp,
-                        //   width: 29.sp,
-                        //   decoration: BoxDecoration(
-                        //     color: Colors.red,
-                        //     borderRadius: BorderRadius.circular(7),
-                        //   ),
-                        //   child:  ClipRRect(
-                        //     borderRadius: BorderRadius.circular(7),
-                        //     child: Image.asset(
-                        //       'assets/images/nft.png',
-                        //       // Replace with your image assets
-                        //       fit: BoxFit.cover,
-                        //     ),
-                        //   ),
-                        // ),
-                        // SizedBox(width: 3.sp,),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            // SizedBox(height: 2.4.h,),
-                            Container(
-                              // color: Colors.red,
-                              width: 35.w,
-                              child: Text(
-                                widget.nftsCollection.collectionName,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                // 'Cube Collection'.tr(),
-                                style: TextStyle(
-                                    color: AppColors.textColorWhite,
-                                    fontWeight: FontWeight.w700,
-                                  fontSize: 10.sp,
-                                  fontFamily: 'Clash Display'),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 0.7.h,
-                            ),
-                            // Row(
-                            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            //   children: [
-                                // Text('6000 SAR',
-                                //     style: TextStyle(
-                                //       color:
-                                //       AppColors.textColorWhite.withOpacity(0.5),
-                                //       fontWeight: FontWeight.w500,
-                                //       fontSize: 7.sp,
-                                //     )),
-                                // Spacer(),
-                            //   ],
-                            // ),
-                          ],
+                  child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+                      child: Container(
+                        height: 7.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10)),
+                          color: AppColors.textColorBlack.withOpacity(0.3),
                         ),
-                      ],
-                    ),
-                  ),
-                )),
-            // Positioned(
-            //     bottom: 10,
-            //     right: 10,
-            //     child:
-            // GestureDetector(
-            //     onTap: () => setState(() {
-            //       _isFavourite = !_isFavourite;
-            //     }),
-            //     child: _isFavourite
-            //         ? Icon(
-            //       Icons.favorite,
-            //       color: AppColors.errorColor,
-            //       size: 13.sp,
-            //     )
-            //         : Icon(
-            //       Icons.favorite_border,
-            //       color: AppColors.textColorGreyShade2,
-            //       size: 13.sp,
-            //     ))
-            // ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 5.sp, right: 5.sp),
+                          child: Row(
+                            // mainAxisAlignment: MainAxisAlignment.start,
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Container(
+                              //   height: 29.sp,
+                              //   width: 29.sp,
+                              //   decoration: BoxDecoration(
+                              //     color: Colors.red,
+                              //     borderRadius: BorderRadius.circular(7),
+                              //   ),
+                              //   child:  ClipRRect(
+                              //     borderRadius: BorderRadius.circular(7),
+                              //     child: Image.asset(
+                              //       'assets/images/nft.png',
+                              //       // Replace with your image assets
+                              //       fit: BoxFit.cover,
+                              //     ),
+                              //   ),
+                              // ),
+                              // SizedBox(width: 3.sp,),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  // SizedBox(height: 2.4.h,),
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        height: 4.5.h,
+                                        width: 4.5.h,
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(
+                                              5.sp),
+                                          child: Image.network(
+                                            'https://images.pexels.com/photos/11881429/pexels-photo-11881429.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+                                            // Path to your placeholder image
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        decoration: BoxDecoration(
+                                          // color: Colors.red,
+                                          borderRadius: BorderRadius.circular(
+                                              5.sp), // Adjust the radius as needed
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 5.sp,
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.only(top: 2.sp),
+                                        // color: Colors.yellow,
+                                        width: 25.w,
+                                        child: Text(
+                                          widget.nftsCollection.collectionName,
+                                          overflow: TextOverflow.ellipsis,
+                                          // 'Cube Collection'.tr(),
+                                          style: TextStyle(
+                                              color: AppColors.textColorWhite,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 10.sp,
+                                              fontFamily: 'Clash Display'),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 0.7.h,
+                                  ),
+                                  // Row(
+                                  //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  //   children: [
+                                  // Text('6000 SAR',
+                                  //     style: TextStyle(
+                                  //       color:
+                                  //       AppColors.textColorWhite.withOpacity(0.5),
+                                  //       fontWeight: FontWeight.w500,
+                                  //       fontSize: 7.sp,
+                                  //     )),
+                                  // Spacer(),
+                                  //   ],
+                                  // ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      )),
+                  // Positioned(
+                  //     bottom: 10,
+                  //     right: 10,
+                  //     child:
+                  // GestureDetector(
+                  //     onTap: () => setState(() {
+                  //       _isFavourite = !_isFavourite;
+                  //     }),
+                  //     child: _isFavourite
+                  //         ? Icon(
+                  //       Icons.favorite,
+                  //       color: AppColors.errorColor,
+                  //       size: 13.sp,
+                  //     )
+                  //         : Icon(
+                  //       Icons.favorite_border,
+                  //       color: AppColors.textColorGreyShade2,
+                  //       size: 13.sp,
+                  //     ))
+                  // ),
+                ))
           ],
         ),
       ),
--- lib/widgets/nfts_design.dart
+++ lib/widgets/nfts_design.dart
@@ -26,128 +26,131 @@ class _NftsDesignState extends State<NftsDesign> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () =>
-          Navigator.of(context).pushNamed(NftsDetails.routeName,
-              arguments: {
-                'tokenName': widget.nfts.tokenName,
-                'tokenId': widget.nfts.id,
-                'creatorId': widget.nfts.creatorId,
-                'image' : widget.nfts.tokenURI,
-                'creatorRoyalty': widget.nfts.creatorRoyalty,
-                'ownerId': widget.nfts.ownerId,
-                // 'nftIds': widget.nftsCollection.nftIds.length.toString(),
-                'standard': widget.nfts.standard.toString(),
-                'chain': widget.nfts.chain.toString(),
-                'createdAt': widget.nfts.createdAt.toString(),
-                'status': widget.nfts.status.toString(),
-                'listingType': widget.nfts.listingType.toString(),
-                'isListable': widget.nfts.isListable.toString(),
-              }
-          ),
+          Navigator.of(context).pushNamed(NftsDetails.routeName, arguments: {
+        'tokenName': widget.nfts.tokenName,
+        'tokenId': widget.nfts.id,
+        'creatorId': widget.nfts.creatorId,
+        'image': widget.nfts.tokenURI,
+        'creatorRoyalty': widget.nfts.creatorRoyalty,
+        'ownerId': widget.nfts.ownerId,
+        // 'nftIds': widget.nftsCollection.nftIds.length.toString(),
+        'standard': widget.nfts.standard.toString(),
+        'chain': widget.nfts.chain.toString(),
+        'createdAt': widget.nfts.createdAt.toString(),
+        'status': widget.nfts.status.toString(),
+        'listingType': widget.nfts.listingType.toString(),
+        'isListable': widget.nfts.isListable.toString(),
+      }),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: Stack(
           children: [
-         Container(
+            Container(
               // decoration: BoxDecoration(color: AppColors.textColorGreyShade2.withOpacity(0.25)),
               height: 27.2.h,
               width: double.infinity,
-              child:   Image.network(
+              child: Image.network(
                 widget.nfts.tokenURI,
                 fit: BoxFit.cover,
                 width: double.infinity,
-                errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+                errorBuilder: (BuildContext context, Object exception,
+                    StackTrace? stackTrace) {
                   return Image.network(
-                    'https://cdn-icons-png.flaticon.com/512/6298/6298900.png', // Path to your placeholder image
+                    'https://cdn-icons-png.flaticon.com/512/6298/6298900.png',
+                    // Path to your placeholder image
                     fit: BoxFit.cover,
                   );
                 },
               ),
             ),
-
             Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-
-                  // child: BackdropFilter(
-                  //   filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-                    child:
-               Container(
-                  height: 7.2.h,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
-                    color: AppColors.textColorBlack.withOpacity(0.7),
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 10.sp, right: 10.sp),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          widget.nfts.tokenName,
-                          overflow: TextOverflow.ellipsis,
-                          // widget.nfts.tokenName,
-                          // 'Neo Cube#812'.tr(),
-                          style: TextStyle(
-                              color: AppColors.textColorWhite,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 10.sp,
-                          fontFamily: 'Clash Display',
+                  child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+                      child: Container(
+                        height: 7.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10)),
+                          color: AppColors.textColorBlack.withOpacity(0.3),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 10.sp, right: 10.sp),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                widget.nfts.tokenName,
+                                overflow: TextOverflow.ellipsis,
+                                // widget.nfts.tokenName,
+                                // 'Neo Cube#812'.tr(),
+                                style: TextStyle(
+                                  color: AppColors.textColorWhite,
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 10.sp,
+                                  fontFamily: 'Clash Display',
+                                ),
+                              ),
+                              SizedBox(
+                                height: 0.2.h,
+                              ),
+                              Text(
+                                widget.nfts.status!,
+                                // widget.nfts.tokenName,
+                                // 'Neo Cube#812'.tr(),
+                                style: TextStyle(
+                                    color: AppColors.nftsSubtitle,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 9.sp),
+                              ),
+                              SizedBox(
+                                height: 0.1.h,
+                              ),
+                              // Row(
+                              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              //   children: [
+                              //     Text(
+                              //         // widget.nfts.tokenId,
+                              //         widget.nfts.price + " SAR",
+                              //         style: TextStyle(
+                              //           color:
+                              //               AppColors.textColorWhite.withOpacity(0.5),
+                              //           fontWeight: FontWeight.w500,
+                              //           fontSize: 7.sp,
+                              //         )),
+                              //     Spacer(),
+                              //     GestureDetector(
+                              //         onTap: () => setState(() {
+                              //               _isFavourite = !_isFavourite;
+                              //             }),
+                              //         child: _isFavourite
+                              //             ? Icon(
+                              //                 Icons.favorite,
+                              //                 color: AppColors.errorColor,
+                              //                 size: 12.sp,
+                              //               )
+                              //             : Icon(
+                              //                 Icons.favorite_border,
+                              //                 color: AppColors.textColorGreyShade2,
+                              //                 size: 12.sp,
+                              //               ))
+                              //   ],
+                              // ),
+                            ],
                           ),
                         ),
-                        SizedBox(
-                          height: 0.2.h,
-                        ),
-                        Text(
-                          widget.nfts.status!,
-                          // widget.nfts.tokenName,
-                          // 'Neo Cube#812'.tr(),
-                          style: TextStyle(
-                              color: AppColors.nftsSubtitle,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 9.sp),
-                        ),
-                        SizedBox(
-                          height: 0.1.h,
-                        ),
-                        // Row(
-                        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //   children: [
-                        //     Text(
-                        //         // widget.nfts.tokenId,
-                        //         widget.nfts.price + " SAR",
-                        //         style: TextStyle(
-                        //           color:
-                        //               AppColors.textColorWhite.withOpacity(0.5),
-                        //           fontWeight: FontWeight.w500,
-                        //           fontSize: 7.sp,
-                        //         )),
-                        //     Spacer(),
-                        //     GestureDetector(
-                        //         onTap: () => setState(() {
-                        //               _isFavourite = !_isFavourite;
-                        //             }),
-                        //         child: _isFavourite
-                        //             ? Icon(
-                        //                 Icons.favorite,
-                        //                 color: AppColors.errorColor,
-                        //                 size: 12.sp,
-                        //               )
-                        //             : Icon(
-                        //                 Icons.favorite_border,
-                        //                 color: AppColors.textColorGreyShade2,
-                        //                 size: 12.sp,
-                        //               ))
-                        //   ],
-                        // ),
-                      ],
-                    ),
-                  ),
-                )
-            //       )
+                      )
+                      //       )
+                      )),
             )
           ],
         ),
